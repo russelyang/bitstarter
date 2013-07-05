1f242bfad07fbcc97eda7efb9c62b649c0c272fd
russelyang <russelyang@gmail.com>
2013-07-04 21:34:34 -0700

    part3

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs');
+var stringFromFile = fs.readFileSync('./index.html').toString();
+
+app.get('/', function(request, response) {
+  response.send(stringFromFile);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
